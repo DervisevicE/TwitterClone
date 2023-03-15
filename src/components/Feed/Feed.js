005fea48017e757af79e590c42d79fad0ebf2ba8
DervisevicE <edervisevi2@etf.unsa.ba>
2023-03-15 12:32:14 +0100

    fixing small details for feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NewPost from "../NewPost/NewPost";
 import Tweet from "../Tweet/Tweet";
+import './Feed.css';
 
 
 const Feed = (props) => {
     return (
         <div className="feed">
+            <h2>Home</h2>
             <NewPost />
             {props.tweets.map((tweet) => (
                 <Tweet key={tweet.id} tweet={tweet} />
