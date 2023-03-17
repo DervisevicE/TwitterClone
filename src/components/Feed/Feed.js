7518c45db1ebe4644f8f04aec5db25338b8927d1
DervisevicE <edervisevi2@etf.unsa.ba>
2023-03-17 16:05:20 +0100

    Adding new tweet

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import NewPost from "../NewPost/NewPost";
 import Tweet from "../Tweet/Tweet";
 import './Feed.css';
 
 
 const Feed = (props) => {
+
+    const [allTweets, setTweets] = useState(props.tweets);
+
+    const addTweetHandler = (tweet) => {
+        setTweets(prevTweets => {
+            return [tweet, ...prevTweets];
+        });
+    }
     return (
         <div className="feed">
             <h2>Home</h2>
-            <NewPost />
-            {props.tweets.map((tweet) => (
+            <NewPost onAddTweet={addTweetHandler} />
+            {allTweets.map((tweet) => (
                 <Tweet key={tweet.id} tweet={tweet} />
             ))}
         </div>
--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import './NewPost.css';
 import Avatar from '../Avatar/Avatar';
-const NewPost = () => {
+const NewPost = (props) => {
+
+    const [enteredText, setEnteredText] = useState("");
+
+    const addTweetHandler = (event) => {
+        event.preventDefault();
+
+        const newTweet = {
+            id: "3",
+            user: "Active User",
+            username: "@active.user",
+            content: enteredText,
+            likes: 0,
+            comments: 0,
+            reposts: 0
+        };
+
+        props.onAddTweet(newTweet);
+    }
+
+    const enteredTextHandler = (event) => {
+        setEnteredText(event.target.value);
+    }
+
     return (
         <div className="new_post">
             <Avatar />
-            <form className="new_tweet" >
-                <input className="new_post_text" type="text" name="name" placeholder="What's happening" />
+            <form className="new_tweet" onSubmit={addTweetHandler}>
+                <input className="new_post_text" type="text" name="text" placeholder="What's happening" onChange={enteredTextHandler} />
                 <input className="new_post_tweet" type="submit" value="Tweet" />
             </form>
         </div>
