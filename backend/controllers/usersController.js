b1d549c1c34055b4cf95fabea944fb51efffc9cf
Esma Dervišević <edervisevi2@etf.unsa.ba>
2023-05-10 21:57:48 +0200

    creating JSON Web Token for sign up

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,11 +1,21 @@
 const { default: mongoose } = require('mongoose')
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+}
+
 
 const signupUser = async (req, res) => {
     const { username, email, password } = req.body
     try {
         const user = await User.signup(username, email, password)
-        res.status(202).json({username, user})
+
+        const token = createToken(user._id)
+
+
+        res.status(202).json({ username, token })
     } catch (error) {
         res.status(404).json({ mssg: error.message })
     }
